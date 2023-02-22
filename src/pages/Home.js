44bb03f104fc3a5103cb18802cb003633a0e7ac2
Raghav Agrawal <raghav.agrawal@spinny.com>
2023-02-22 05:33:36 +0530

    readme file update

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import "../App.css"
+
+const Home = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="container"></div>
+    </>
+  );
+};
+
+export default Home;
